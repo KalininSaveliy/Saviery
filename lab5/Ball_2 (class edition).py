00a96adaadbac9fcf94fba113e9ae3693fb6e831
KalininSaveliy <55765626+KalininSaveliy@users.noreply.github.com>
2019-11-06 01:25:23 +0300

    Add files via upload

--- lab5/Ball_2 (class edition).py
+++ lab5/Ball_2 (class edition).py
@@ -1,6 +1,7 @@
 from tkinter import *
 from random import randrange as rnd, choice
 
+
 a = 800
 b = 600
 colors = ['red', 'orange', 'yellow', 'green', 'blue']
@@ -8,30 +9,61 @@ root = Tk()
 root.geometry(str(a) + 'x' + str(b))
 canv = Canvas(root, bg='white')
 canv.pack(fill=BOTH, expand=1)
+score = 0
+text = canv.create_text(50, 50, text='Score: ' + str(score))
+kol_balls = 500     # количество шаров
+balls = []
 
 
 class Ball:
-    x = rnd(100, a - 100)
-    y = rnd(100, b - 100)
-    r = rnd(30, 50)
-    dx = rnd(-7, 7)
-    dy = rnd(-7, 7)
-    color = choice(colors)
-    canv.create_oval(x - r, y - r, x + r, y + r, fill=color, width=0)
+    def __init__(self):
+        self.x = rnd(100, a - 100)
+        self.y = rnd(100, b - 100)
+        self.r = rnd(30, 50)
+        self.dx = rnd(-5, 5)
+        self.dy = rnd(-5, 5)
+        self.color = choice(colors)
+        self.id = canv.create_oval(self.x - self.r, self.y - self.r,
+                                   self.x + self.r, self.y + self.r,
+                                   fill=self.color, width=0)
 
     def move(self):
-        print(self.r)
-        #canv.move(self.ball, self.dx, self.dy)
+        if self.x - self.r < 0 or self.x + self.r > a:
+            self.dx *= -1
+        if self.y - self.r < 0 or self.y + self.r > a:
+            self.dy *= -1
+        canv.move(self.id, self.dx, self.dy)
+        self.x += self.dx
+        self.y += self.dy
 
-    def chek_inside(self):
-        pass
+    def check_inside(self, event):
+        if (self.x - event.x) ** 2 + (self.y - event.y) ** 2 < self.r ** 2:
+            return True
+        else:
+            return False
+
+
+def move_balls():
+    dt = 1
+    for i in balls:
+        i.move()
+    root.after(dt, move_balls)
+
+
+def click(event):
+    global score, text
+    for i in balls:
+        if i.check_inside(event):
+            score += 1
+            canv.delete(text)
+            text = canv.create_text(50, 50, text='Score: ' + str(score))
+
 
 def main():
-    ball = Ball
-    print(ball.r)
-    for i in range(100):
-        ball.move()     # i do not know, please help with this f line
+    for i in range(kol_balls):
+        balls.append(Ball())
+    move_balls()
+    canv.bind('<Button>', click)
     mainloop()
-
 
 main()
